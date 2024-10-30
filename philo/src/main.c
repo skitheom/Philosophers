816e4fc1e2e41893a928b650ec59042c129176f9
skitheom <SahoKitahara@my.uopeople.edu>
2024-10-30 20:16:28 +0900

    .

--- philo/src/main.c
+++ philo/src/main.c
@@ -1,21 +1,40 @@
 #include "philo.h"
 
+int	convert_input_to_num(const char *str)
+{
+	char	*endptr;
+	long	value;
+
+	if (!str || !*str)
+		return (-1);
+	endptr = NULL;
+	errno = 0;
+	value = strtol(str, &endptr, 10);
+	if (errno != 0 || *endptr != '\0' || value > INT_MAX || value < 0)
+		return (-1);
+	return ((int)value);
+}
+
 static bool	validate_argv(int argc, const char **argv)
 {
+	int	i;
+
 	if (argc != 5 && argc != 6)
 	{
 		print_error(ERR_USAGE);
 		return (false);
 	}
-	if (convert_input_to_num(argv[1]) <= 0 || convert_input_to_num(argv[2]) <= 0
-		|| convert_input_to_num(argv[3]) <= 0
-		|| convert_input_to_num(argv[4]) <= 0 || (argv[5]
-			&& convert_input_to_num(argv[5]) <= 0))
+	i = 1;
+	while (i < argc)
 	{
-		print_error(ERR_INVALID_INPUT);
-		return (false);
+		if (convert_input_to_num(argv[i]) < 0 || (i != 5
+				&& convert_input_to_num(argv[i]) == 0))
+		{
+			print_error(ERR_INVALID_INPUT);
+			return (false);
+		}
 	}
-	return (true);
+	return (i == argc);
 }
 
 int	main(int argc, const char **argv)
@@ -23,16 +42,18 @@ int	main(int argc, const char **argv)
 	t_ctrl	ctrl;
 
 	if (!validate_argv(argc, argv))
-	{
-		cleanup_ctrl(&ctrl);
-		return (1);
-	}
+		return (EXIT_FAILURE);
 	if (!init_ctrl(&ctrl, argc, argv))
 	{
+		print_error(ERR_INIT);
+		return (EXIT_FAILURE);
+	}
+	if (!launch_threads(&ctrl, ctrl.philos, ctrl.config.num_of_philos))
+	{
+		print_error(ERR_LAUNCH_THREAD);
 		cleanup_ctrl(&ctrl);
-		print_error(ERR_MALLOC);
-		return (1);
+		return (EXIT_FAILURE);
 	}
 	cleanup_ctrl(&ctrl);
-	return (0);
+	return (EXIT_SUCCESS);
 }
