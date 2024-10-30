d948a48182813bf9bef44314607b8163e0aaef75
skitheom <SahoKitahara@my.uopeople.edu>
2024-10-30 20:08:49 +0900

    added error_flag

--- philo/src/init.c
+++ philo/src/init.c
@@ -12,6 +12,33 @@ static void	setup_config(t_config *config, int argc, const char **argv)
 		config->num_of_times_to_eat = -1;
 }
 
+static bool	init_mutex_locks(t_ctrl *ctrl)
+{
+	if (pthread_mutex_init(&ctrl->dead_lock, NULL) != 0)
+	{
+		return (false);
+	}
+	if (pthread_mutex_init(&ctrl->meal_lock, NULL) != 0)
+	{
+		pthread_mutex_destroy(&ctrl->dead_lock);
+		return (false);
+	}
+	if (pthread_mutex_init(&ctrl->write_lock, NULL) != 0)
+	{
+		pthread_mutex_destroy(&ctrl->dead_lock);
+		pthread_mutex_destroy(&ctrl->meal_lock);
+		return (false);
+	}
+	if (pthread_mutex_init(&ctrl->error_lock, NULL) != 0)
+	{
+		pthread_mutex_destroy(&ctrl->dead_lock);
+		pthread_mutex_destroy(&ctrl->meal_lock);
+		pthread_mutex_destroy(&ctrl->write_lock);
+		return (false);
+	}
+	return (true);
+}
+
 static bool	init_mutex_forks(t_ctrl *ctrl)
 {
 	int	i;
@@ -25,32 +52,10 @@ static bool	init_mutex_forks(t_ctrl *ctrl)
 		{
 			while (--i >= 0)
 				pthread_mutex_destroy(&ctrl->forks[i]);
+			destroy_mutex_locks(ctrl);
 			return (false);
 		}
 		i++;
-	}
-	return (true);
-}
-
-static bool	init_mutex_locks(t_ctrl *ctrl)
-{
-	if (pthread_mutex_init(&ctrl->dead_lock, NULL) != 0)
-	{
-		destroy_mutex_forks(ctrl);
-		return (false);
-	}
-	if (pthread_mutex_init(&ctrl->meal_lock, NULL) != 0)
-	{
-		destroy_mutex_forks(ctrl);
-		pthread_mutex_destroy(&ctrl->dead_lock);
-		return (false);
-	}
-	if (pthread_mutex_init(&ctrl->write_lock, NULL) != 0)
-	{
-		destroy_mutex_forks(ctrl);
-		pthread_mutex_destroy(&ctrl->dead_lock);
-		pthread_mutex_destroy(&ctrl->meal_lock);
-		return (false);
 	}
 	return (true);
 }
@@ -69,7 +74,6 @@ static bool	setup_philos(t_ctrl *ctrl)
 		ctrl->philos[i].id = i + 1;
 		ctrl->philos[i].eaten_meals_count = 0;
 		ctrl->philos[i].eating = false;
-		ctrl->philos[i].start_time = current_time;
 		ctrl->philos[i].last_meal = current_time;
 		ctrl->philos[i].left_fork = &(ctrl->forks[i]);
 		if (i == 0)
@@ -89,7 +93,7 @@ bool	init_ctrl(t_ctrl *ctrl, int argc, const char **argv)
 	if (!ctrl->philos)
 		return (false);
 	ctrl->forks = malloc(sizeof(pthread_mutex_t) * ctrl->config.num_of_philos);
-	if (!ctrl->forks || !init_mutex_forks(ctrl) || !init_mutex_locks(ctrl))
+	if (!ctrl->forks || !init_mutex_locks(ctrl) || !init_mutex_forks(ctrl))
 	{
 		cleanup_memory(ctrl);
 		return (false);
@@ -100,5 +104,6 @@ bool	init_ctrl(t_ctrl *ctrl, int argc, const char **argv)
 		return (false);
 	}
 	ctrl->dead_flag = false;
+	ctrl->error_flag = false;
 	return (true);
 }
