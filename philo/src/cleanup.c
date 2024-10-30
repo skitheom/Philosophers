484aa250765ead032a4d18b52eaf590bd9983bd0
skitheom <SahoKitahara@my.uopeople.edu>
2024-10-30 20:15:03 +0900

    error lock added

--- philo/src/cleanup.c
+++ philo/src/cleanup.c
@@ -1,5 +1,13 @@
 #include "philo.h"
 
+void	destroy_mutex_locks(t_ctrl *ctrl)
+{
+	pthread_mutex_destroy(&ctrl->dead_lock);
+	pthread_mutex_destroy(&ctrl->meal_lock);
+	pthread_mutex_destroy(&ctrl->write_lock);
+	pthread_mutex_destroy(&ctrl->error_lock);
+}
+
 void	destroy_mutex_forks(t_ctrl *ctrl)
 {
 	int	i;
@@ -14,24 +22,21 @@ void	destroy_mutex_forks(t_ctrl *ctrl)
 	}
 }
 
-void	destroy_mutex_locks(t_ctrl *ctrl)
+void	xfree(void *ptr)
 {
-	pthread_mutex_destroy(&ctrl->dead_lock);
-	pthread_mutex_destroy(&ctrl->meal_lock);
-	pthread_mutex_destroy(&ctrl->write_lock);
+	if (ptr)
+		free(ptr);
 }
 
 void	cleanup_memory(t_ctrl *ctrl)
 {
-	if (ctrl->philos)
-		free(ctrl->philos);
-	if (ctrl->forks)
-		free(ctrl->forks);
+	xfree(ctrl->philos);
+	xfree(ctrl->forks);
 }
 
 void	cleanup_ctrl(t_ctrl *ctrl)
 {
-	destroy_mutex_forks(ctrl);
 	destroy_mutex_locks(ctrl);
+	destroy_mutex_forks(ctrl);
 	cleanup_memory(ctrl);
 }
